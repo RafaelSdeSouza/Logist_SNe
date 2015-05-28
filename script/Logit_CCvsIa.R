a672a08c6b02b4aaf59503a087612e29873f64dc
Rafael <rafael.2706@gmail.com>
2015-05-28 22:22:30 +0100

    new plots

--- script/Logit_CCvsIa.R
+++ script/Logit_CCvsIa.R
@@ -12,49 +12,52 @@ library(MASS)
 library(scales)
 library(plyr)
 require(runjags)
-#Read clean dataset
+#Read the already clean dataset
 
 data.0= read.table(file="..//data//clean_cat.dat",header=TRUE)
 
 galtype<-match(SN_cat4$Galtype2,c("S","S0","E/S0","E","Im"))
 Ntype<-length(unique(SN_cat4$Galtype2))
 
-X<-model.matrix(~ Galtype2-1, data = SN_cat4)
-K<-ncol(X)
 
 typeSne<-as.numeric(SN_cat4$SNtype)-1
 jags.data <- list(Y= typeSne,
                  N = nrow(SN_cat4),
-                 X = X,
-                 b0 = rep(0, K),
-                 B0 = diag(0.00001, K))
+                 mag_g = data.0$mag_g,
+                 galtype = galtype,
+                 Ntype=Ntype
+                 )
 
 
 model<-"model{
 #1. Priors
+
 tau.R<-pow(sdBeta,-1)
 sdBeta ~ dgamma(0.01,0.01)
 
-for(j in 1:5){
-beta[j] ~ ddexp(0,tau.R)
+# Random intercept 
+for (j in 1:Ntype){
+ranef[j]~ddexp(0,tau.R)
 }
-beta.0~dnorm(0,1e-6)
+
+beta.0~dnorm(0,0.001)
+beta.1~dnorm(0,0.001)
+
 #2. Likelihood
 for (i in 1:N){
 Y[i] ~ dbern(p[i])
 logit(p[i]) <-  eta[i]
-eta[i] <- beta.0+inprod(beta[], X[i,])
+eta[i] <- beta.0+beta.1*mag_g[i]+ranef[galtype[i]]
 #3. Prediction
 prediction[i]~dbern(p[i])
 }
 }"
-inits<-function () {
-  list(beta = rnorm(K, 0, 0.1))}
-params <- c("beta","prediction")
 
-inits1=inits()
-inits2=inits()
-inits3=inits()
+params <- c("beta.0","beta.1","prediction","ranef")
+
+inits1=list(beta.0=rnorm(1,0,0.1),beta.1=rnorm(1,0,0.1))
+inits2=list(beta.0=rnorm(1,0,0.1),beta.1=rnorm(1,0,0.1))
+inits3=list(beta.0=rnorm(1,0,0.1),beta.1=rnorm(1,0,0.1))
 
 library(parallel)
 cl <- makeCluster(3)
@@ -66,7 +69,7 @@ jags.logit <- run.jags(method="rjparallel", method.options=list(cl=cl),
                      adapt=2500,
                      monitor=c(params),
                      burnin=20000,
-                     sample=30000,
+                     sample=40000,
                      summarise=FALSE,
                      thin=5,
                      plots=FALSE
@@ -78,19 +81,19 @@ summary<-extend.jags(jags.logit,drop.monitor=c("prediction"), summarise=TRUE)
 
 require(ggmcmc)
 L.factors <- data.frame(
-  Parameter=paste("beta[", seq(1:6), "]", sep=""),
-  Label=c("beta.0","S","S0","E/S0","E","Im"))
+  Parameter=paste("ranef[", seq(1:5), "]", sep=""),
+  Label=c("S","S0","E/S0","E","Im"))
 head(L.factors)
-beta_post<-ggs(jagssamples,par_labels=L.factors,family=c("beta"))
+ranef_post<-ggs(jagssamples,par_labels=L.factors,family=c("ranef"))
 
 
-pdf("..//figures/betas.pdf",width=7,height=7)
-ggs_caterpillar(beta_post)+theme_stata()+ylab("")
+pdf("..//figures/ranef.pdf",width=7,height=7)
+ggs_caterpillar(ranef_post)+theme_stata()+ylab("")
 dev.off()
 
 
-pdf("..//figures/density.pdf",width=7,height=10)
-ggs_density(beta_post)+theme_stata()+ylab("")
+pdf("..//figures/density2.pdf",width=7,height=10)
+ggs_density(ranef_post)+theme_stata()+ylab("")
 dev.off()
 
 
