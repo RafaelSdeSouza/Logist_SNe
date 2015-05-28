77b1115c1c96a7ccd4cc94a2e23be7eff0c48b69
Rafael <rafael.2706@gmail.com>
2015-05-28 12:28:57 +0100

    new script

--- script/Logit_CCvsIa.R
+++ script/Logit_CCvsIa.R
@@ -10,7 +10,7 @@ library(Cairo)
 library(plyr)
 library(MASS)
 library(scales)
-
+library(plyr)
 #Read dataset
 
 data.0= read.fwf(file="..//data//snsdss.dat",width = c(7,1, 11, 9, 1,9,1,8,2,1,
@@ -26,8 +26,10 @@ SN_cat2<-na.omit(SN_cat)
 require(gdata)
 SN_cat2$SNtype<-trim(SN_cat2$SNtype)
 
-SN_cat3<-SN_cat2[which(SN_cat2$SNtype=="Ia"|SN_cat2$SNtype=="II"),]
+SN_cat3<-SN_cat2[which(SN_cat2$SNtype=="Ia"|SN_cat2$SNtype=="II"|SN_cat2$SNtype=="Ia"|
+                         SN_cat2$SNtype=="Ib"|SN_cat2$SNtype=="Ib/c"|SN_cat2$SNtype=="Ic"),]
 SN_cat3$SNtype<-droplevels(SN_cat3$SNtype)
+SN_cat3$SNtype<-revalue(SN_cat3$SNtype,c("Ia"="Ia","Ib"="CC","Ib/c"="CC","Ic"="CC","II"="CC"))
 
 # Start the logit model
 
@@ -37,7 +39,7 @@ SN_cat3$SNtype<-droplevels(SN_cat3$SNtype)
 
 
 SN_cat3$Galtype<-trim(SN_cat3$Galtype)
-library(plyr)
+
 SN_cat3$Galtype2<-revalue(SN_cat3$Galtype, c("E pec" ="E", "E/S0 pec"="E/S0","S pec"="S","S0 pec"="S0",
                            "S0/a"="S0","S0/a pec"="S0","Sa"="S","Sa pec"="S","Sab"="S","Sab pec"="S","Sb"="S","Sb pec"="S",
                            "Sbc"="S","Sbc pec"="S","Sc"="S","Sc pec"="S","Scd"="S",
