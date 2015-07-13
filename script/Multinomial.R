25271898b5d81ba5e4c2edb2b5abcb7e4919d04d
Rafael <rafael.2706@gmail.com>
2015-07-13 08:15:30 +0200

    ok

--- script/Multinomial.R
+++ script/Multinomial.R
@@ -1,4 +1,3 @@
-
 #  Required libraries
 library(rjags)
 library(ggmcmc)
@@ -38,6 +37,7 @@ model<- "model{
     y[i] ~ dcat(p[i, 1:J])
     
     for (j in 1:J){
+      log(q[i,j]) <-  beta[1,j] + beta[2,j]*bar[i]+ranef[galtype[i],j]
       
       p[i,j] <- q[i,j]/sum(q[i,1:J])
     }   # close J loop
@@ -46,7 +46,16 @@ model<- "model{
 
 # Priors
 
+#tau.R<-pow(sdBeta,-1)
+#sdBeta ~ dgamma(0.001,0.001)
 
+# Random intercept model
+for (k in 1:Ntype){
+#ranef[j]~dnorm(0,1e-5)
+ranef[k,1]~dbern(0)
+for(j in 2:J){
+ranef[k,j]~ddexp(0,1/1.64)
+}
 }
 
   for(k in 1:2){
@@ -82,7 +91,7 @@ jags.mlogit <- run.jags(method="rjparallel",
                        n.chains = 3,
                        adapt=1000,
                        monitor=c(params),
-                       burnin=1000,
+                       burnin=2000,
                        sample=5000,
                        summarise=FALSE,
                        plots=FALSE
@@ -93,12 +102,32 @@ print(summary)
 jagssamples <- as.mcmc.list(jags.mlogit)
 L.factors <- data.frame(
   Parameter=c(paste("beta[1,", seq(1:6), "]", sep=""),paste("beta[2,", seq(1:6), "]", sep="")),
+  Label=c("Ia","Ib","Ib/c","Ic","II",
+        "IIn","Bar-Ia","Bar-Ib","Bar-Ib/c","Bar-Ic","Bar-II",
+        "Bar-IIn"))
 #head(L.factors)
 
 beta_post<-ggs(jagssamples,par_labels=L.factors,family=c("beta"))
 pdf("..//figures/multi_beta.pdf",width=8,height=10)
 ggs_caterpillar(beta_post)+theme_stata()+ylab("")
 dev.off()
+
+L.factors2<-data.frame(
+  Parameter=c(paste("ranef[1,",seq(1:6), "]", sep=""),paste("ranef[2,",seq(1:6), "]", sep=""),paste("ranef[3,",seq(1:6), "]", sep=""),
+              paste("ranef[4,",seq(1:6), "]", sep=""),paste("ranef[5,",seq(1:6), "]", sep="")),
+  Label=c("E-Ia","E-Ib","E-Ibc","E-Ic","E-II","E-IIn","E/S0-Ia","E/S0-Ib","E/S0-Ibc","E/S0-Ic","E/S0-II","E/S0-IIn",
+          "S-Ia","S-Ib","S-Ibc","S-Ic","S-II","S-IIn","S0-Ia","S0-Ib","S0-Ibc","S0-Ic","S0-II","S0-IIn",
+          "Im-Ia","Im-Ib","Im-Ibc","Im-Ic","Im-II","Im-IIn"),
+  supernova=as.factor(rep(c("Ia","Ib","Ibc","Ic","II","IIn"),5)),galaxies=as.factor(rep(c("E","E/S0","S","S0","Im"),each=6)))
+
+cof_post<-ggs(jagssamples,par_labels=L.factors2,family="ranef")
+pdf("..//figures/multi_ranef.pdf",width=12,height=20)
+ggs_caterpillar(cof_post)+theme_stata()+ylab("")
+dev.off()
+
+
+
+
 
 # probabilities
 prob<-summary(as.mcmc.list(jags.mlogit, vars="p"))
